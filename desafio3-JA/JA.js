cf55697f809afab0f7f82140794a86d950d1dcc8
maurivarela002 <mauricio.varela@2innovateit.com>
2021-08-03 15:15:43 -0300

    Desafio 3

--- desafio3-JA/JA.js
+++ desafio3-JA/JA.js
@@ -1,6 +1,15 @@
-function ReciboTexto (texto, cb){
-    for (let palabra = "Hola"; palabra = texto.includes(palabra);) {
-        console.log(`La palabra "${palabra}" ${texto.includes(palabra) ? 'esta' : 'no esta'} en el texto`);
+function ReciboTexto(num, texto, cb) {
+    let palabra = "Hola"
+    let arrayPalabras = texto.split();
+    console.log(arrayPalabras.length)
+    for (var i = 0; i < arrayPalabras.length; i++) {
+        console.log(texto);
     }
+    setTimeout(cb, 2500)
 }
-ReciboTexto("Hola como estas")
+ReciboTexto(1,"Hola como estas, tienes mandarinas 🍊🍊🍊🍊?", ()=>{
+    ReciboTexto(2,"Bien y tu, si tengo 🍊, cuantas Quieres?", ()=>{
+        ReciboTexto(3,"1k⚖️, gracias!", ()=>{
+        });
+    })
+})
