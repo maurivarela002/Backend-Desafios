0201856f5e03640bbf788c3d7708a36802fff142
maurivarela002 <mauricio.varela@2innovateit.com>
2021-08-10 19:29:41 -0300

    nodemon

--- desafio 5/index.js
+++ desafio 5/index.js
@@ -1,19 +1,19 @@
 const http = require("http");
-function numAleatorio(min, max) {
-    let number;
-    return number = Math.floor(min + Math.random() * (max - min + 1));
+function numeroAleatorio(min, max) {
+  let number;
+  return (number = Math.floor(min + Math.random() * (max - min + 1)));
 }
-const objeto = {
-    id: `${numAleatorio(1,10)}`,
-    title: "Producto" + `${numAleatorio(1,10)}`,
-    price: `${numAleatorio(0,9999.99)}`,
-    thumbnail: "Foto" + `${numAleatorio(1,10)}`
-}
+const obj = {
+  id: `${numeroAleatorio(1, 10)}`,
+  title: "Producto" + `${numeroAleatorio(1, 10)}`,
+  price: `${numeroAleatorio(0, 9999.99)}`,
+  thumbnail: "Foto" + `${numeroAleatorio(1, 10)}`,
+};
 const server = http.createServer((request, response) => {
-    response.end("Hola mundo!");
-    response.end(JSON.stringify(objeto));
+  response.end("Hola , deberias estar viendo el server!");
+  response.end(JSON.stringify(obj));
 });
-const port = 6000;
+const port = 6400;
 server.listen(port, () => {
-    console.log(`Servidor escuchando en ${port}`);
+  console.log(`Servidor escuchando en ${port}`);
 });
