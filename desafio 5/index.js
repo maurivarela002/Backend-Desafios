563eb053fa14a0de37b337ced9c7468dcdae8154
maurivarela002 <mauricio.varela@2innovateit.com>
2021-08-11 14:13:19 -0300

    mejora desafio 5

--- desafio 5/index.js
+++ desafio 5/index.js
@@ -1,21 +1,21 @@
 const http = require("http");
 
 function numeroAleatorio(min, max) {
-  let number;
-  return (number = Math.floor(min + Math.random() * (max - min + 1)));
+  return Math.floor(min + Math.random() * (max - min + 1));
 }
-const obj = {
+
+let obj = {
   id: `${numeroAleatorio(1, 10)}`,
   title: "Producto" + `${numeroAleatorio(1, 10)}`,
   price: `${numeroAleatorio(0, 9999.99)}`,
   thumbnail: "Foto" + `${numeroAleatorio(1, 10)}`,
 };
+
 const server = http.createServer((request, response) => {
-  const objetoPort = response.end(JSON.stringify(obj));
-  console.log(objetoPort);
+  response.end(JSON.stringify(obj));
 });
 
-const port = 4000;
+const port = 3000;
 server.listen(port, () => {
   console.log(`Servidor escuchando en ${port}`);
 });
