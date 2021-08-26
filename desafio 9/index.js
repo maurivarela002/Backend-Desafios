36055debf81ec7896e952d1122997efcbe537666
Mauri Varela <mvarela4617@gmail.com>
2021-08-26 12:34:57 -0300

    desafio 9

--- desafio 9/index.js
+++ desafio 9/index.js
@@ -1,14 +1,43 @@
-import express from 'express';
-import path from 'path';
-const __dirname = path.resolve();
-import fs from 'fs';
-const app = express();
-const router = express.Router();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use('/api', router);
+import express from "express";
+import path from "path";
+import productos from "./productos.js";
 
+
+const __dirname = path.resolve();
 const port = 8080;
-app.listen(port, () => {
-    console.log(`Puerto ${port} levantado!`)
+const app = express();
+const server = app.listen(port, () => {
+    console.log(`Puerto ${port} levantado!`);
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+
+server.on("error", (error) => {
+    console.error(error);
+});
+
+//parte1 put
+app.put('/api/productos/actualizar/:id', (req, res) => {
+    try {
+        const producto = productos.actualizarProducto(req.body.title, req.body.price, req.body.thumbnail, req.params.id);
+        if (producto) {
+            res.send(producto);
+            return;
+        } else {
+            res.send({ error: 'producto no actualizado' });
+        }
+    } catch (err) {
+        console.log("hubo un error al actualizar", err);
+    }
+});
+
+//parte3
+app.delete('/api/productos/eliminar/:id', (req, res) => {
+    const producto = productos.borrarProducto(req.body.title, req.body.price, req.body.thumbnail, req.params.id);
+    res.send(producto);
+});
+
+// app.get("/api", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
